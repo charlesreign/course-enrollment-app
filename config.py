0e029a0e45c0ad3be7579acb7b67d2934da3b249
charlie <charlesgold45@gmail.com>
2022-02-19 12:48:19 +0000

    configure the app with the database

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "f9bf78b9a18ce6d46a0cd2b0b86df9da"
+
+    MONGODB_SETTINGS = {'db': 'Enrollment',
+    'host': 'mongodb://localhost:27017/Enrollment'
+    }
